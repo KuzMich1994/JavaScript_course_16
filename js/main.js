aaca20ae5222c189e0ea9cc1e5e6b8f7d9c225d1
KuzMich1994 <drumer19940310@mail.ru>
2020-11-20 14:10:57 +0300

    Выполнил урок №2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 'use strict';
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-
-alert('Урок №1! Поехали!');
-console.log('Урок №1! Собственно приехали!)');
+let money = 50000;
+console.log(typeof ('money: ', money));
+let income = 'Фриланс';
+console.log(typeof ('income: ', income));
+let addExpenses = 'Еда, такси, интернет, всякая всячина';
+console.log(addExpenses.length);
+let deposit = true;
+console.log(typeof ('deposit: ', deposit));
+let mission = 999999999999;
+let period = 11;
+console.log(`Период равен ${period} месяцев` + `
+Цель заработать ${mission} долларов`);
+console.log(addExpenses.toLowerCase());
+let budgetDay = money / 30;
+console.log(budgetDay);
