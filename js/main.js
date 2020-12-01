0794e8408e8b290f875cfa23e206423c1ec31968
KuzMich1994 <drumer19940310@mail.ru>
2020-12-01 18:13:41 +0300

    Выполнил задание к уроку № 9

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 'use strict';
+const startButton = document.getElementById('start'),
+buttonPluse1 = document.getElementsByTagName('button')[0],
+buttonPluse2 = document.getElementsByTagName('button')[1],
+depositCheck = document.querySelector('#deposit-check'),
+additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
+budgetMonthValue = document.getElementsByClassName('budget_month-value'),
+budgetDayValue = document.getElementsByClassName('budget_day-value'),
+expensesMonthValue = document.getElementsByClassName('expenses_month-value'),
+additionalIncomeValue = document.getElementsByClassName('additional_income-value'),
+additionalExpensesValue = document.getElementsByClassName('additional_expenses-value'),
+incomePeriodValue = document.getElementsByClassName('income_period-value'),
+targetMonthValue = document.getElementsByClassName('target_month-value'),
+salaryAmount = document.querySelector('.salary-amount'),
+incomeTitle = document.querySelector('input.income-title'),
+incomeAmount = document.querySelector('.income-amount'),
+expensesTitle = document.querySelector('input.expenses-title'),
+expensesAmount = document.querySelector('.expenses-amount'),
+additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+depositAmount = document.querySelector('.deposit-amount'),
+depositPercent = document.querySelector('.deposit-percent'),
+targetAmount = document.querySelector('.target-amount'),
+periodSelect = document.querySelector('.period-select');
 
 const isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
@@ -141,6 +163,4 @@ console.log(appData.getStatusIncome());
   }
 appData.getInfoDeposit();
 console.log(appData.percentDeposit, appData.moneyDeposit, appData.calcSavedMoney());
-// console.log(appData.addExpenses.join(', ').charAt(0).toLocaleUpperCase() + 
-// appData.addExpenses.join(', ').slice(1));
 
