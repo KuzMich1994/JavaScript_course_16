b46ef370e1c430a5a00d48a07da296420bbc2f5a
KuzMich1994 <drumer19940310@mail.ru>
2020-12-18 02:18:10 +0300

    Исправил ошибку с ссылками заглушками

--- js/main.js
+++ js/main.js
@@ -126,7 +126,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // кнопка скролла
   const scroll = () => {
-    const anchors = document.querySelectorAll('a[href*="#"]');
+    const anchors = document.querySelectorAll('menu>ul>li>a[href]'),
+      scrollButton = document.querySelector('main>a');
+
+    scrollButton.addEventListener('click', event => {
+      event.preventDefault();
+
+      const blockID = scrollButton.getAttribute('href').substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
 
     anchors.forEach(anchor => {
       anchor.addEventListener('click', event => {
